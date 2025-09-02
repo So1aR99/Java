58f457ad1d3eff674fe915f3a4fe59b251a9c685
노태성 <taesung_no@naver.com>
2025-09-02 10:17:12 +0900

    Create Cat.java

---
+++ 0902/OOPTest04/Cat.java
@@ -0,0 +1,34 @@
+public class Cat {
+
+    private String name;
+    private int age;
+    private String color;
+
+    public Cat(){
+    }
+    public Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public Cat(String name, int age, String color){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public void Meow(){
+        System.out.println("야옹 야옹");
+    }
+    public void Eat(){
+        System.out.println("냠냠");
+    }
+
+    // 직접 접근, 간접 접근
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
