283f4898ae2c52f620fa6d1bea5322eaf4eb167e
노태성 <taesung_no@naver.com>
2025-09-03 11:24:43 +0900

    인터페이스

---
+++ 0903/InterfaceTest01.java
@@ -0,0 +1,26 @@
+class Horse{
+    public void run() {
+        System.out.println("말처럼 달리다.");
+    }
+}
+interface IWing {
+    public void fly();
+}
+interface IVersionChina {
+}
+interface IVersionJapan {
+}
+
+class Unicorn extends Horse implements IWing, IVersionJapan{
+    @Override
+    public void fly() {
+        System.out.println("새처럼 날다.");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Unicorn unicorn = new Unicorn();
+        unicorn.run();
+        unicorn.fly();
+    }
+}
