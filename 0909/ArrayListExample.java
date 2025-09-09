340a0c0340634cdfe2933568467005b3476f7e0c
노태성 <taesung_no@naver.com>
2025-09-09 09:38:42 +0900

    Create ArrayListExample.java

---
+++ 0909/ArrayListExample.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+
+        list.add("Java");
+        list.add("JDBC");
+        list.add("Servlet/JSP");
+        list.add(2, "database");    // 2번 인덱스에 있던게 3번으로 옮겨지고 2번인덱스에 삽입
+        list.add("iBATIS");
+
+        int size = list.size();
+        System.out.println("총 객체 수 : " + size);
+        System.out.println();
+
+        for(int i = 0; i < list.size(); i++) {
+            String str = list.get(i);
+            System.out.println(i + ": " + str);
+        }
+        System.out.println();
+
+        list.remove(2);
+        list.remove(2);
+        list.remove("iBATIS");
+        for(int i = 0; i < list.size(); i++) {
+            String str = list.get(i);
+            System.out.println(i + ": " + str);
+        }
+    }
+}
