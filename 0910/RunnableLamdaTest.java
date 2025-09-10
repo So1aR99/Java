b8e77506ea7819809f329b0921f5f616173bf8b1
노태성 <taesung_no@naver.com>
2025-09-10 10:12:50 +0900

    람다 줄이기

---
+++ 0910/RunnableLamdaTest.java
@@ -0,0 +1,20 @@
+class A implements Runnable {
+    @Override
+    public void run() {
+        for(int i=1; i<=100; i++)
+            System.out.println(i);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        Thread t1 = new Thread(a);
+        t1.start();
+
+        // 람다 10줄을 줄임
+        new Thread(() -> {
+            for(int i=1; i<=100; i++)
+                System.out.println(i);}).start();
+    }
+}
