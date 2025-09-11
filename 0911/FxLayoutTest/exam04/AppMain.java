6b27217f4098679e9343e74d6e2e3781c380a77c
노태성 <taesung_no@naver.com>
2025-09-11 11:47:42 +0900

    Update AppMain.java

--- 0911/FxLayoutTest/exam04/AppMain.java
+++ 0911/FxLayoutTest/exam04/AppMain.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Launcher extends Application {
+public class AppMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
